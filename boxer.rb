e22e761b00113cb4d095b84c45e54c08df98316f
Evan Bottcher <evan@bottch.com>
2020-06-06 07:58:51 +1000

    Made a json to puml thing

--- boxer.rb
+++ boxer.rb
@@ -2,16 +2,49 @@
 
 require "json"
 
+class Box
+
+  def initialize json_box
+    @id = json_box["id"]
+    @name = json_box["name"]
+    @type = json_box["type"]
+    @boxes = json_box["boxes"] ? json_box["boxes"].collect { |child_json_box| Box.new child_json_box } : []
+  end
+
+  def print file
+    file.write("Boundary(#{@id}, \"#{@name}\", \"#{@type}\") {\n")
+    @boxes.each { |box| box.print file }
+    file.write("}\n")
+  end
+
+end
+
+class Puml 
+
+  def initialize json_boxes
+    @boxes = json_boxes.collect { |json_box| Box.new json_box}
+  end
+
+  def print filename
+
+    File.open(filename, 'w') do |file|
+      file.write("@startuml Example 2\n")
+      file.write("!includeurl https://raw.githubusercontent.com/RicardoNiepel/C4-PlantUML/release/1-0/C4_Container.puml\n\n")
+
+      @boxes.each { |box| box.print file }
+
+      file.write("@enduml\n")
+    end
+  end
+
+end
+
 json_from_file = File.read("sample.json")
 parsed_json = JSON.parse(json_from_file)
 
-File.open("sample.puml", 'w') do |file|
-  file.write("@startuml Example 2\n")
-  file.write("!includeurl https://raw.githubusercontent.com/RicardoNiepel/C4-PlantUML/release/1-0/C4_Container.puml\n\n")
+boxes = parsed_json["data"]["boxes"]
 
-  # stuff goes here.
+puml = Puml.new boxes
+puml.print "sample.puml"
 
-  file.write("@enduml\n")
-end
 
-#TODO - run file through plantuml to export png
