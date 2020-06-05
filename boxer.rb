648fa5347fa8a4402c862aa1a06fbedb7ba23839
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-06-05 23:15:05 +1000

    start and end of file

--- boxer.rb
+++ boxer.rb
@@ -2,10 +2,16 @@
 
 require "json"
 
-
 json_from_file = File.read("sample.json")
 parsed_json = JSON.parse(json_from_file)
 
-#TODO - Parse into puml format
-#TODO - write file`
+File.open("sample.puml", 'w') do |file|
+  file.write("@startuml Example 2\n")
+  file.write("!includeurl https://raw.githubusercontent.com/RicardoNiepel/C4-PlantUML/release/1-0/C4_Container.puml\n\n")
+
+  # stuff goes here.
+
+  file.write("@enduml\n")
+end
+
 #TODO - run file through plantuml to export png
