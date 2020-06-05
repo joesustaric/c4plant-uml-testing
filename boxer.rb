525fc49367b3c1a764cc87acb0761227331aafde
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-06-05 23:05:27 +1000

    todos

--- boxer.rb
+++ boxer.rb
@@ -6,4 +6,6 @@ require "json"
 json_from_file = File.read("sample.json")
 parsed_json = JSON.parse(json_from_file)
 
-puts parsed_json
+#TODO - Parse into puml format
+#TODO - write file`
+#TODO - run file through plantuml to export png
